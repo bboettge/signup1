54c94d0ac9865ce53c7c99c5c4184dff3324fc7b
Bram <bramboettge@gmail.com>
2016-08-23 22:15:21 -0500

    second commit

--- main.py
+++ main.py
@@ -16,10 +16,95 @@
 #
 import webapp2
 
+
+def validate_username(username):
+    if len(username) > 0:
+        return username
+
+def validate_password(password):
+    if len(password):
+        return password
+
+def validate_verify_password(password):
+    return password
+
+def validate_email(email):
+    return email
+
+
+form = """
+<form action="/welcome" method="post">
+    <h1>Signup</h1>
+    <label>Username:
+        <input name="username"/><div><font color="red">%(error_username)s</font></div>
+    </label>
+    <br>
+    <br>
+
+    <label>Password:
+        <input name="password"/><div><font color="red">%(error_password)s</font></div>
+    </label>
+    <br>
+    <br>
+
+    <label>Verify password:
+        <input name="verify_password"/><div><font color="red">%(error_verify_password)s</font></div>
+    </label>
+    <br>
+    <br>
+
+    <label>Email (optional):
+        <input name="email"/><div><font color="red">%(error_email)s</font></div>
+    </label>
+    <br>
+    <br>
+
+    <label>
+        <input type="submit">
+    </label>
+</form>
+"""
+
+
+
 class MainHandler(webapp2.RequestHandler):
+
+    def write_form(self, error_username="", error_password="", error_verify_password="", error_email=""):
+        self.response.out.write(form % {"error_username": error_username,
+                                        "error_password": error_password,
+                                        "error_verify_password": error_verify_password,
+                                        "error_email": error_email})
+
     def get(self):
-        self.response.write('Hello world!')
+        self.write_form()
+
+class Welcome_User(webapp2.RequestHandler):
+    def write_form(self, error_username="", error_password="", error_verify_password="", error_email=""):
+        self.response.out.write(form % {"error_username": error_username,
+                                        "error_password": error_password,
+                                        "error_verify_password": error_verify_password,
+                                        "error_email": error_email})
+
+    def post(self):
+        username = validate_username(self.request.get("username"))
+        password = validate_password(self.request.get("password"))
+        verify_password = validate_verify_password(self.request.get("verify_password"))
+        email = validate_email(self.request.get("email"))
+
+        if not username:
+            self.write_form("That is not a valid username.")
+        elif not password:
+            self.write_form("", "That is not a valid password.")
+        elif not verify_password:
+            self.write_form("", "", "Passwords are not the same.")
+        elif not email:
+            self.write_form("", "", "", "That is not a valid email")
+        else:
+            self.response.out.write("Welcome, " + username + "!")
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/welcome', Welcome_User)
 ], debug=True)
